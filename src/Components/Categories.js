45992f20b3d594510ca14199800b5f26650ec2f8
Vance Leon <vanceleon44@gmail.com>
2019-08-09 16:49:23 -0400

    listing out the categories

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,9 +1,10 @@
 import React, {useReducer} from 'react';
 import { reducer, initialState } from '../reducer';
-import categories from '../dummyData';
+import {categories} from '../dummyData';
 
 export function Categories() {
     // const [state, dispatch ] = useReducer(reducer, initialState);
+    console.log(typeof(categories));
     return (
         <div className='categories-container'>
             {categories.map(cat => {
