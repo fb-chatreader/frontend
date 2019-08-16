766f21bd23f361319c2aeccc884d1eb642933366
A.D.Faris <adfaris@gmail.com>
2019-08-15 20:25:32 -0400

    termsOfService and Privacy policy

--- src/Components/RecommendationPage.js
+++ src/Components/RecommendationPage.js
@@ -1,12 +1,19 @@
-import React, { useReducer } from 'react'
-import { initialState, reducer } from '../reducer'
+import React, { useReducer } from 'react';
+import { initialState, reducer } from '../reducer';
+import {recommentations} from '../dummyData';
 
-export default function RecommendationPage () {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
+export function RecommendationPage () {
+  // const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <div>
       <h1>Recommendation Page</h1>
+      <div>
+        {recommentations.map(recommentation => {
+          return(
+            <div>{recommentation}</div>
+          )
+        })}
+      </div>
     </div>
   )
 }
