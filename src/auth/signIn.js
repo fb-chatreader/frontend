3a3d2f684db75cdc11d9f3a9c63e5b3a15ac4764
Vance Leon <vanceleon44@gmail.com>
2019-08-19 16:41:16 -0400

    signIn component

--- src/auth/signIn.js
+++ src/auth/signIn.js
@@ -1,2 +1,7 @@
 import React, {Component } from 'react';
 
+// import {compose} from 'recompose' need to look into this
+import {withRouter} from 'react-router-dom';
+import {Form, Button, Header, Icon, Segment} from 'semantic-ui-react';
+
+import {SignUpLink} from './signUp.js';
