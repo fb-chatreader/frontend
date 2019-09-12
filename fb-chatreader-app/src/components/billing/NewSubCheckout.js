a80b58018435e0e180f7fbed5ad646ccba2ca5dc
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-09-11 19:48:43 -0700

    Added axios post with subData to NeSubCheckout submit method

--- fb-chatreader-app/src/components/billing/NewSubCheckout.js
+++ fb-chatreader-app/src/components/billing/NewSubCheckout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardElement, injectStripe } from 'react-stripe-elements';
+import axios from 'axios';
 
 class NewSubCheckout extends Component {
   constructor(props) {
@@ -7,8 +8,25 @@ class NewSubCheckout extends Component {
     this.submit = this.submit.bind(this);
   }
 
-  async submit(ev) {
-    // User clicked submit
+  async submit(event) {
+    let { token } = await this.props.stripe.createToken({ name: "New Subscription" });
+
+    // let response = await fetch("/charge", {
+    //   method: "POST",
+    //   headers: {"Content-Type": "text/plain"},
+    //   body: token.id
+    // });
+    const subData = {
+      source: token.id
+    };
+    
+    axios.post('/api/billing/checkout/newsub', subData)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
   }
 
   render() {
@@ -16,7 +34,7 @@ class NewSubCheckout extends Component {
       <div className="new-sub-checkout-form">
         <p>Would you like to complete the purchase?</p>
         <CardElement />
-        <button onClick={this.submit}>Send</button>
+        <button onClick={this.submit}>Submit</button>
       </div>
     );
   }
