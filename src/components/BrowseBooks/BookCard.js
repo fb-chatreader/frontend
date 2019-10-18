ba0308587c0182fe7bb0bf05a42e966e15ae1851
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-10-18 14:21:23 -0700

    changed summary button to div with styling to look like button

--- src/components/BrowseBooks/BookCard.js
+++ src/components/BrowseBooks/BookCard.js
@@ -12,13 +12,13 @@ function BookCard(props) {
       <div className={styles.footer}>
         <h4 className={styles.authorText}>By {props.book.author}</h4>
         <div className={styles.cardActions}>
-          <button className={styles.summaryBtn}
+          <div className={styles.summaryBtn}
             type='button'
             onClick={() =>
               window.open(`${process.env.REACT_APP_MESSENGER_URL}?ref=command=start_book,book_id=${props.book.id}`)}
           >
             Read Summary!
-          </button>
+          </div>
           <ShareBook data={props} />
         </div>
       </div>
