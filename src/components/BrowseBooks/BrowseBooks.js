1deecfa83f90ca3be64c22b4bc876df303f06ea8
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-22 20:14:44 -0400

    Removes unused route

--- src/components/BrowseBooks/BrowseBooks.js
+++ src/components/BrowseBooks/BrowseBooks.js
@@ -4,37 +4,34 @@ import axios from 'axios';
 import { BookContext } from 'Providers/BooksProvider.js';
 
 import BrowseCategories from './BrowseCategories';
-import BookPage from './BookPage';
 import styles from '../../scss/components/BrowseBooks.module.scss';
 
 function BrowseBooks(props) {
-  const [ state, dispatch ] = useContext(BookContext);
-  const [ categories, setCategories ] = useState([]);
+  const [state, dispatch] = useContext(BookContext);
+  const [categories, setCategories] = useState([]);
 
-  useEffect(
-    () => {
-      if (!state.books || !state.books.length) {
-        axios.get('/api/books/').then((res) => {
-          setCategories(Array.from(new Set(res.data.map(({ category }) => category))).sort());
-          dispatch({ type: 'POPULATE_BOOKS', payload: res.data });
-          dispatch({
-            type: 'POPULATE_CATEGORIES',
-            payload: categories
-          });
+  useEffect(() => {
+    if (!state.books || !state.books.length) {
+      axios.get('/api/books/').then(res => {
+        setCategories(
+          Array.from(new Set(res.data.map(({ category }) => category))).sort()
+        );
+        dispatch({ type: 'POPULATE_BOOKS', payload: res.data });
+        dispatch({
+          type: 'POPULATE_CATEGORIES',
+          payload: categories
         });
-      }
-    },
-    [ state.books, state.categories, dispatch ]
-  );
+      });
+    }
+  }, [state.books, categories, dispatch]);
 
   return (
     <div className={styles.browseBooks}>
       <Route exact path="/browse">
-        {categories && <BrowseCategories categories={categories} books={state.books} />}
+        {categories && (
+          <BrowseCategories categories={categories} books={state.books} />
+        )}
       </Route>
-      {/* <Route path="/summary/:id">
-        <BookPage />
-      </Route> */}
     </div>
   );
 }
