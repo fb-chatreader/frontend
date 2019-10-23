560c2f67f7c2fa98e05b23bf95acc33df1df1fd1
Erik Kimsey <kimseyerik@gmail.com>
2019-10-23 12:32:56 -0400

    Finding selected book in BookPage

--- src/components/BrowseBooks/BookPage.js
+++ src/components/BrowseBooks/BookPage.js
@@ -3,16 +3,27 @@ import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Switch, useParams } from 'react-router-dom';
 // import BookGrid from './BooksGrid';
 import SingleBook from './SingleBook';
-// import { BookContext } from 'Providers/BooksProvider.js';
+import { BookContext } from 'Providers/BooksProvider.js';
 
 const BookPage = (props) => {
   const { id } = useParams();
+  const [ state, dispatch ] = useContext(BookContext);
   console.log(id);
-  
+  const _id = parseInt(id);
+
+  // console.log(state.books[id]);
+  const books = [ ...state.books ];
+  // console.log(books[0]);
+  const book = books.find((e, i) => {
+    if (books[i].id === _id) {
+      return books[i];
+    }
+  });
+
   return (
     <div style={{ width: '100%', height: ' 600px' }}>
-      BOOK PAGE
-      <SingleBook />
+      
+      <SingleBook book={book} />
     </div>
   );
 };
