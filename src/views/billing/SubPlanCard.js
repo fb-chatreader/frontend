b4dc24ef01c1834360273d3478157c30d5f4e1a5
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-10-25 16:46:22 -0700

    Formatted code in SubPlanCard

--- src/views/billing/SubPlanCard.js
+++ src/views/billing/SubPlanCard.js
@@ -22,19 +22,14 @@ function SubPlanCard(props) {
                 <p>You are subscribed to this plan</p>
             ) : (
                 props.id_token && 
-
-                
-                <Link
-                    exact
-                    to={`/checkout/newsub/${props.id_token}/${props.plan.nickname}/${props.plan.id}`}
-                >
-                    <button className={styles.selectPlanBtn}>
-                        Select Plan
-                    </button>
-                </Link>
-
-
-                
+                    <Link
+                        exact
+                        to={`/checkout/newsub/${props.id_token}/${props.plan.nickname}/${props.plan.id}`}
+                    >
+                        <button className={styles.selectPlanBtn}>
+                            Select Plan
+                        </button>
+                    </Link>  
             )}
         </div>
     )
