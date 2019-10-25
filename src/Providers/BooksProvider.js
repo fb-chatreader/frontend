f49f1c92743010e3b005c1bd8557e9c3198360dc
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-10-25 16:52:32 -0700

    Added getState method to BooksProvider and set Provider value={getState}

--- src/Providers/BooksProvider.js
+++ src/Providers/BooksProvider.js
@@ -1,10 +1,22 @@
 import React, { useReducer, createContext } from 'react';
 import { reducer, initialState } from './reducers/books.js';
+import axios from 'axios';
 
 export const BookContext = createContext();
 
 export function BooksProvider(props) {
   const { Provider } = BookContext;
 
-  return <Provider value={useReducer(reducer, initialState)}>{props.children}</Provider>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const getState = () => {
+    if (!state.books.length) {
+      axios
+        .get('/api/books/')
+        .then(res => dispatch({ type: 'POPULATE_BOOKS', payload: res.data }));
+    }
+    return [state, dispatch];
+  };
+
+  return <Provider value={getState}>{props.children}</Provider>;
 }
