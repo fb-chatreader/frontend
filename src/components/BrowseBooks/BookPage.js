22e2a131539316921e4c98909e74b7f8c4c846b6
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-24 20:26:02 -0400

    Updates to use state function

--- src/components/BrowseBooks/BookPage.js
+++ src/components/BrowseBooks/BookPage.js
@@ -6,9 +6,9 @@ import { BookContext } from 'Providers/BooksProvider.js';
 import styles from '../../scss/components/BookPage.module.scss';
 const BookPage = () => {
   const { id } = useParams();
-  const [ state ] = useContext(BookContext);
+  const [state] = useContext(BookContext)();
   const _id = parseInt(id);
-  const books = [ ...state.books ];
+  const books = [...state.books];
   const book = books.find((e, i) => {
     if (books[i].id === _id) {
       return books[i];
--- src/components/BrowseBooks/BrowseBooks.js
+++ src/components/BrowseBooks/BrowseBooks.js
@@ -1,34 +1,24 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
-import axios from 'axios';
 import { BookContext } from 'Providers/BooksProvider.js';
 
 import BrowseCategories from './BrowseCategories';
 import styles from '../../scss/components/BrowseBooks.module.scss';
 
 function BrowseBooks() {
-  const [ state, dispatch ] = useContext(BookContext);
+  const [state, dispatch] = useContext(BookContext)();
 
-  useEffect(
-    () => {
-      if (!state.books || !state.books.length) {
-        axios.get('/api/books/').then((res) => {
-          const categories = Array.from(new Set(res.data.map(({ category }) => category))).sort();
-          dispatch({ type: 'POPULATE_BOOKS', payload: res.data });
-          dispatch({
-            type: 'POPULATE_CATEGORIES',
-            payload: categories
-          });
-        });
-      }
-    },
-    [ state.books, state.categories, dispatch ]
-  );
-  
+  useEffect(() => {
+    if (!state.books || !state.books.length) {
+    }
+  }, [state.books, state.categories, dispatch]);
+
   return (
     <Route exact path="/browse">
       <div className={styles.browseBooks}>
-        {state.categories && <BrowseCategories categories={state.categories} books={state.books} />}
+        {state.categories && (
+          <BrowseCategories categories={state.categories} books={state.books} />
+        )}
       </div>
     </Route>
   );
