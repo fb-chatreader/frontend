bd1942b50e594b17fe93bc5afe4888cbacd59f3c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-24 20:25:08 -0400

    Combines populate books and categories

--- src/Providers/reducers/books.js
+++ src/Providers/reducers/books.js
@@ -1,20 +1,24 @@
 export const initialState = {
   books: [],
   categories: [],
-  selectedBook:null,
-  selectedCategory:null
+  selectedBook: null,
+  selectedCategory: null
 };
 
 export function reducer(state, action) {
   switch (action.type) {
     case 'POPULATE_BOOKS':
-      return { ...state, books: action.payload };
-    case 'POPULATE_CATEGORIES':
+      return {
+        ...state,
+        books: action.payload,
+        categories: Array.from(
+          new Set(action.payload.map(({ category }) => category))
+        ).sort()
+      };
+    case 'SELECTED_BOOK':
       return { ...state, categories: action.payload };
-      case 'SELECTED_BOOK':
-        return { ...state, categories: action.payload };
-      case 'SELECTED_CATEGORY':
-        return { ...state, selectedCategory: action.payload };
+    case 'SELECTED_CATEGORY':
+      return { ...state, selectedCategory: action.payload };
     default:
       return state;
   }
