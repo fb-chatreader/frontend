998dffd16c1ba0c9bfcf80ff40db658d1f5c225f
Erik Kimsey <kimseyerik@gmail.com>
2019-10-28 09:28:26 -0400

    Added material-ui icons and created constants file/obj with breakpoint values

--- src/navigation/NavBar.js
+++ src/navigation/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import NavLinksNonAuth from './NavLinksNonAuth';
 import styles from './styles/NavBar.module.scss';
+import IconButton from '@material-ui/core/IconButton';
+import RESP_DEVICE_WIDTHS from '../styles/constants';
 
 // Currently NavBar only renders NavNonAuth
 // If we implement authentication, we can create a NavAuth with different links and styling
@@ -10,6 +12,10 @@ import styles from './styles/NavBar.module.scss';
 const NavBar = () => (
   <div className={styles.navbar}>
     <div className={styles.navBarContents}>
+      {/* 
+      Use RESP_DEVICE_WIDTHS.tabletLandscape as breakpoint for 
+      displaying menu icon.
+    */}
       <h1 className={styles.appName}>Chatwise</h1>
       <NavLinksNonAuth />
     </div>
