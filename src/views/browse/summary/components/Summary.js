5808115056f8102dd3bba5646fe33eca8c5a375e
Erik Kimsey <kimseyerik@gmail.com>
2019-10-30 17:43:43 -0400

    Tinkered with adding book cover to summaryview

--- src/views/browse/summary/components/Summary.js
+++ src/views/browse/summary/components/Summary.js
@@ -3,8 +3,10 @@ import styles from '../styles/Summary.module.scss';
 const Summary = (props) => {
   return (
     <div className={styles.summaryContainer}>
-      <div className={styles.bookTitle}>{props.book.title}</div>
-      <div className={styles.bookAuthor}>{props.book.author}</div>
+      <div className={styles.headerContent}>
+        <div className={styles.bookTitle}>{props.book.title}</div>
+        <div className={styles.bookAuthor}>{props.book.author}</div>
+      </div>
       <div className={styles.bookSynopsis}>{props.book.synopsis}</div>
     </div>
   );
