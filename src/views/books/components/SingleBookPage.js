2ec1e428dcd419cc699859eaf4696faa3f84736a
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-10-31 17:52:57 -0700

    finished basic SingleBookPage modeled after BookCard.

--- src/views/books/components/SingleBookPage.js
+++ src/views/books/components/SingleBookPage.js
@@ -1,41 +1,45 @@
 import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { BookContext } from 'Providers/BooksProvider';
 // import { Link } from 'react-router-dom';
 import ShareBook from './ShareBook';
-import styles from '../styles/BookCard.module.scss';
+import styles from '../styles/SingleBookPage.module.scss';
 
-function SingleBookPage(props) {
+const SingleBookPage= () => {
     const [state] = useContext(BookContext)();
-    const { bookId } = useParams();
-    const book = state.books.filter(b => b.id === bookId);
-
-    return (
+    const id = parseInt(useParams().id, 10);
+    const book = state.books.filter(b => b.id === id)[0];
+    console.log(book);
+    return book ? ( 
         <div className={styles.SingleBookPage}>
-        <div className={styles.header}>
-            <img
-            className={styles.bookImage}
-            src={book.image_url}
-            alt="book-cover"
-            />
-            <h2 className={styles.titleText}>{book.title}</h2>
-        </div>
-        <div className={styles.footer}>
-            <h4 className={styles.authorText}>By {book.author}</h4>
-            <div className={styles.cardActions}>
-            <div className={styles.summaryBtn}>
-                <button
-                onClick={() =>
-                    window.open(
-                    `${process.env.REACT_APP_MESSENGER_URL}?ref=command=start_book,book_id=${book.id}`
-                    )
-                }
-                >
-                Read Summary!
-                </button>
+            <div className={styles.header}>
+                <img
+                    className={styles.bookImage}
+                    src={book.image_url}
+                    alt="book-cover"
+                />
+                <h2 className={styles.titleText}>{book.title}</h2>
             </div>
-            <ShareBook book_id={book.id} />
+            <div className={styles.footer}>
+                <h4 className={styles.authorText}>By {book.author}</h4>
+                <div className={styles.cardActions}>
+                <div className={styles.summaryBtn}>
+                    <button
+                    onClick={() =>
+                        window.open(
+                        `${process.env.REACT_APP_MESSENGER_URL}?ref=book_id=${book.id}`
+                        )
+                    }
+                    >
+                    Read Summary!
+                    </button>
+                </div>
+                <ShareBook book_id={book.id} />
+                </div>
             </div>
         </div>
-        </div>
+    ) : (
+        <p>No book found</p>
     );
 }
 
