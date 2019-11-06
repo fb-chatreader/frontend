f765dbc7b99719d7ac2c045d2b8daac1bfe3a792
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-11-06 16:34:13 -0500

    Copies from props now

--- src/util/CopyLinkButton.js
+++ src/util/CopyLinkButton.js
@@ -1,40 +1,36 @@
 import React, { useState } from 'react';
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import LinkIcon from "@material-ui/icons/Link";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import LinkIcon from '@material-ui/icons/Link';
 
 import styles from './styles/CopyLinkButton.module.scss';
 
-
 const CopyLinkButton = props => {
-    const [copied, setCopied] = useState(false);
+  const [copied, setCopied] = useState(false);
 
-    const handleCopy = () => {
-        setCopied(true);
-        setTimeout(() => { setCopied(false) }, 3000);
-    }
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
 
-    return (
-        <div className={styles.CopyLinkButton}>
-            {copied &&
-            <div className={styles.copiedModal}>
-                Copied to clipboard
-            </div>
-            }
-            <CopyToClipboard
-                    text={`${process.env.REACT_APP_MESSENGER_URL}?ref=book_id=${props.book_id}`}
-                    onCopy={() => {
-                        // alert('Copied to clipboard');
-                        handleCopy();
-                    }}
-                    title='Copy link'
-                >
-                    <button className={styles.linkBtn}>
-                        <LinkIcon />
-                    </button>
-            </CopyToClipboard>
-        </div>
-    );
-
+  return (
+    <div className={styles.CopyLinkButton}>
+      {copied && <div className={styles.copiedModal}>Copied to clipboard</div>}
+      <CopyToClipboard
+        text={props.toCopy}
+        onCopy={() => {
+          // alert('Copied to clipboard');
+          handleCopy();
+        }}
+        title="Copy link"
+      >
+        <button className={styles.linkBtn}>
+          <LinkIcon />
+        </button>
+      </CopyToClipboard>
+    </div>
+  );
 };
 
 export default CopyLinkButton;
