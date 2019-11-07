b6d84ca07bb680e8a05278f96521e9777b09d762
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-11-06 16:53:37 -0800

    Added starter text snippets to social share buttons. See ShareBook.js for details.

--- src/views/books/components/ShareBook.js
+++ src/views/books/components/ShareBook.js
@@ -14,17 +14,19 @@ import styles from '../styles/ShareBook.module.scss';
 
 function ShareBook(props) {
   const shareUrl = `${process.env.REACT_APP_MESSENGER_URL}?ref=book_id=${props.book_id}`;
-
+  const socialQuote = 'Check out this book on ChatWise';
+  const emailSubject = 'A book I thought you would like';
+  const emailBody = 'I found this book on ChatWise and I think you might like it. You can read a quick summary right in the ChatWise messenger!';
   return (
     <div className={styles.btnContainer}>
       <CopyLinkButton />
-      <FacebookShareButton url={shareUrl} className={styles.iconBtn}>
+      <FacebookShareButton url={shareUrl} quote={socialQuote} className={styles.iconBtn}>
         <FacebookIcon size={32} round={true} />
       </FacebookShareButton>
-      <TwitterShareButton url={shareUrl} className={styles.iconBtn}>
+      <TwitterShareButton url={shareUrl} title={socialQuote} className={styles.iconBtn}>
         <TwitterIcon size={32} round={true} />
       </TwitterShareButton>
-      <EmailShareButton url={shareUrl} className={styles.iconBtn}>
+      <EmailShareButton url={shareUrl} subject={emailSubject} body={emailBody} className={styles.iconBtn}>
         <EmailIcon size={32} round={true} />
       </EmailShareButton>
       <div className="sms-icon-button">
