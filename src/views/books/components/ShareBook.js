f17ed4a2452fad4cebb74941e4d609e1e78efac4
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-11-08 20:44:49 -0500

    Resolves bug from merge

--- src/views/books/components/ShareBook.js
+++ src/views/books/components/ShareBook.js
@@ -20,27 +20,23 @@ function ShareBook(props) {
   return (
     <div className={styles.btnContainer}>
       <CopyLinkButton toCopy={shareUrl} />
-      <FacebookShareButton url={shareUrl} className="icon-button">
+      <FacebookShareButton url={shareUrl} className='icon-button'>
         <FacebookIcon size={32} round={true} />
       </FacebookShareButton>
-      <TwitterShareButton
-        url={shareUrl}
-        title={socialQuote}
-        className={styles.iconBtn}
-      >
+      <TwitterShareButton url={shareUrl} className={styles.iconBtn}>
         <TwitterIcon size={32} round={true} />
       </TwitterShareButton>
       <EmailShareButton
         url={shareUrl}
         subject={`[Book Summary] ${title} by ${author}`}
         body={`Hi, I thought you might like this books summary of ${title} by ${author}\n\nRead Book Summary:`}
-        className="icon-button"
+        className='icon-button'
         openWindow={true}
       >
         <EmailIcon size={32} round={true} />
       </EmailShareButton>
 
-      <div className="sms-icon-button">
+      <div className='sms-icon-button'>
         <SMS url={shareUrl} book={{ title, author }} />
       </div>
     </div>
